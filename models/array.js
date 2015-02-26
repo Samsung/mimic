25a3087c52370e8016d47813ce1f772492776d18
Stefan Heule <stefanheule@gmail.com>
2015-02-26 11:46:45 -0800

    more array models

--- models/array.js
+++ models/array.js
@@ -9,3 +9,32 @@ function push(arg0) {
     var n1 = arg0.length
     return i2+n0
 }
+
+function pop(arg0) {
+    var n0 = arg0.length
+    if (n0) {
+        var n1 = arg0[n0-1]
+        arg0.length = n0-1
+        delete arg0[n0-1]
+        return n1
+    } else {
+        arg0.length = 0
+    }
+}
+
+function shift(arg0) {
+    var n0 = arg0.length
+    if (n0) {
+        var n1 = arg0[0]
+        var n2 = arguments[3]
+        for (var i6 = 0; i6 < (n0-1); i6 += 1) {
+            n2 = arg0[i6+1]
+            arg0[i6] = n2
+        }
+        delete arg0[i6]
+        arg0.length = i6
+        return n1
+    } else {
+        arg0.length = 0
+    }
+}
