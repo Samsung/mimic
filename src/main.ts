1d2a97b935dead074427c53866d7cb5c78abf04f
Manu Sridharan <m.sridharan@samsung.com>
2015-03-11 10:05:46 -0700

    add a simple sum function

--- src/main.ts
+++ src/main.ts
@@ -219,6 +219,14 @@ var fs:any = [
         (a: any[], b) => a.map(b),
         [[1,2,3], (x) => x+1],
     ],
+    [ // 18
+        "our sum function",
+        (a: any[]) => {
+            var result = 0; for (var i = 0; i < a.length; i++) result += a[i]; return result; },
+        [[4,5,6]],
+        [[1,2,3]],
+
+    ],
 ]
 
 
