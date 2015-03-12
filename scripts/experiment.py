d65525e590a9d52ac30f5d7bd8002662584f0d29
Stefan Heule <stefanheule@gmail.com>
2015-03-12 11:53:22 -0700

    restructure experiment code

--- scripts/experiment.py
+++ scripts/experiment.py
@@ -28,7 +28,8 @@ def get_time():
 
 def main():
   parser = argparse.ArgumentParser(description='Run synthesis experiment.')
-  parser.add_argument('-n', type=int, help='Number of repetitions', default=5)
+  parser.add_argument('-n', type=int, help='Number of repetitions', default=10)
+  parser.add_argument('--timeout', type=int, help='Timeout in seconds', default=60)
   parser.add_argument('--filter', type=str, help='Filter which experiments to run', default="")
   parser.add_argument('-r', '--run', help='Only run the first experiment.  Usually used together with --filter', action='store_true')
   parser.add_argument('--verify', help='Verify that all experiments are successful at least some of the time', action='store_true')
@@ -38,6 +39,7 @@ def main():
   workdir = os.path.abspath(os.path.dirname(__file__) + "/../tests")
   n = argv.n
 
+  line = "-" * 80
 
   only_run = argv.run
   verify = argv.verify
@@ -45,77 +47,54 @@ def main():
     print "Cannot both --run and --verify"
     sys.exit(1)
 
+  fncs = parse_functions(workdir, argv.filter)
+  if only_run:
+    f = fncs[0]
+    command = './model-synth synth --cleanup 1000 --iterations 100000000 ' + f.get_command_args()
+    print "Running the example: " + f.title
+    print command
+    print line
+    sys.exit(os.system(command))
+
   # create a directory to store information
-  out = ""
-  if not only_run:
-    out = workdir + "/out"
-    if not os.path.exists(out):
-      os.mkdir(out)
-    out = out + "/" + time.strftime("%Y-%m-%d_%H-%M-%S", time.gmtime())
-    if os.path.exists(out):
-      print "ERROR, out directory exists already: " + out
-      sys.exit(1)
+  out = workdir + "/out"
+  if not os.path.exists(out):
     os.mkdir(out)
+  out = out + "/" + time.strftime("%Y-%m-%d_%H-%M-%S", time.gmtime())
+  if os.path.exists(out):
+    print "ERROR, out directory exists already: " + out
+    sys.exit(1)
+  os.mkdir(out)
 
-  categories = []
-  for f in os.listdir(workdir):
-    if os.path.isfile(workdir + "/" + f):
-      categories.append(f)
-
-  line = "-" * 80
-  for e in categories:
-    category = e[0:-5].replace("/", "-")
-    try:
-      examples = json.loads(open(workdir + "/" + e).read())
-    except ValueError as ex:
-      print "Failed to parse configuration: " + str(ex)
-      sys.exit(1)
-    for example in examples:
-      title = example['name']
-      if argv.filter != "" and re.search(argv.filter, title, re.UNICODE) is None:
-        continue
-      name = title[title.rfind(".")+1:]
-      function = "\n".join(example['function'])
-      argnames = example['argnames']
-      arguments = example['arguments']
-      if not only_run:
-        print line
-        print "Experiment: " + title
-      args = '"' + ('" "'.join(arguments)) + '"'
-      succ_time = 0.0
-      succ_count = 0
-      succ_iterations = 0
-      for i in range(n):
-        more = ""
-        if "loop" in example:
-          more = " --loop " + str(example['loop'])
-        if only_run:
-          command = './model-synth synth ' + more + ' --cleanup 1000 --iterations 100000000 "%s" "%s" %s' % (argnames, function, args)
-          print command
-          os.system(command)
-          sys.exit(0)
-        sys.stdout.write('  Running try #' + str(i+1))
-        sys.stdout.flush()
-        t = time.time()
-        command = './model-synth synth ' + more + ' --out "%s/%s-%s-%d.js" "%s" "%s" %s' % (out, category, name, i, argnames, function, args)
-        val, output = execute(command, 60)
-        elapsed_time = time.time() - t
-        print ". Exit status %d after %.2f seconds." % (val, elapsed_time)
-        if val == 0:
-          succ_count += 1
-          if verify:
-            break
-          succ_time += elapsed_time
-          iters = int([m.group(1) for m in re.finditer('Found in ([0-9]+) iteration', output)][-1])
-          succ_iterations += iters
-      if verify:
-        if succ_count == 0:
-          print "ERROR: didn't succeed :("
-      else:
-        print "Success rate: %.2f%%" % (float(succ_count) * 100.0/float(n))
-        if succ_count > 0:
-          print "Average time until success: %.2f seconds" % (succ_time / float(succ_count))
-          print "Average iterations until success: %.1f" % (float(succ_iterations) / float(succ_count))
+  for f in fncs:
+    print line
+    print "Experiment: " + f.title
+    succ_time = 0.0
+    succ_count = 0
+    succ_iterations = 0
+    for i in range(n):
+      sys.stdout.write('  Running try #' + str(i+1))
+      sys.stdout.flush()
+      t = time.time()
+      command = './model-synth synth --iterations 100000000 --out "%s/%s-%s-%d.js" %s' % (out, f.category, f.name, i, f.get_command_args())
+      val, output = execute(command, argv.timeout)
+      elapsed_time = time.time() - t
+      print ". Exit status %d after %.2f seconds." % (val, elapsed_time)
+      if val == 0:
+        succ_count += 1
+        if verify:
+          break
+        succ_time += elapsed_time
+        iters = int([m.group(1) for m in re.finditer('Found in ([0-9]+) iteration', output)][-1])
+        succ_iterations += iters
+    if verify:
+      if succ_count == 0:
+        print "ERROR: didn't succeed :("
+    else:
+      print "Success rate: %.2f%%" % (float(succ_count) * 100.0/float(n))
+      if succ_count > 0:
+        print "Average time until success: %.2f seconds" % (succ_time / float(succ_count))
+        print "Average iterations until success: %.1f" % (float(succ_iterations) / float(succ_count))
   print line
 
 # print a string to a file
@@ -129,6 +108,51 @@ def fprinta(f, s):
   f = open(f, 'a')
   f.write(s)
   f.close()
+
+# a function that we might want to synthesize
+class Function(object):
+  def __init__(self, data, category):
+    self.category = category
+    self.title = data['name']
+    self.name = self.title[self.title.rfind(".")+1:]
+    self.code = "\n".join(data['function'])
+    self.argnames = data['argnames']
+    self.arguments = data['arguments']
+    if "loop" in data:
+      self.loop = data['loop']
+    else:
+      self.loop = None
+
+  def get_command_args(self):
+    args = '"' + ('" "'.join(self.arguments)) + '"'
+    res = '"%s" "%s" %s' % (self.argnames, self.code, args)
+    if self.loop is None:
+      return res
+    else:
+      return "--loop " + str(self.loop) + " " + res
+
+def parse_functions(workdir, filter = None):
+  """
+  :rtype : list[Function]
+  """
+  result = []
+  categories = []
+  for f in os.listdir(workdir):
+    if os.path.isfile(workdir + "/" + f):
+      categories.append(f)
+
+  for e in categories:
+    category = e[0:-5].replace("/", "-")
+    try:
+      examples = json.loads(open(workdir + "/" + e).read())
+    except ValueError as ex:
+      print "Failed to parse configuration: " + str(ex)
+      sys.exit(1)
+    for example in examples:
+      if filter != None and filter != "" and re.search(filter, example['name'], re.UNICODE) is None:
+        continue
+      result.append(Function(example, category))
+  return result
 
 def execute(cmd, timeout=100000000):
   try:
